a40f908efb70e87d703bfb8fd6ff3e81fa1c65b3
OjElsa <elsahojwang06@gmail.com>
2024-02-01 14:55:37 -0800

    code refactor

--- 13-binary_tree_nodes.c
+++ 13-binary_tree_nodes.c
@@ -9,14 +9,13 @@
 
 size_t binary_tree_nodes(const binary_tree_t *tree)
 {
-	size_t nodes = 0;
-
 	if (tree == NULL)
 		return (0);
 
 	if (tree->left == NULL || tree->right == NULL)
-		nodes += 1;
-	nodes += binary_tree_nodes(tree->left);
-	nodes += binary_tree_nodes(tree->right);
-	return (nodes);
+		return (1 + binary_tree_nodes(tree->left) + binary_tree_nodes
+				(tree->right));
+
+	else
+		return (0);
 }
