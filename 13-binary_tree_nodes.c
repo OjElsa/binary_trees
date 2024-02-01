d5b102394def25305d3a755cd82c616c4dba8e81
OjElsa <elsahojwang06@gmail.com>
2024-02-01 15:53:08 -0800

    wqcode refactor

--- 13-binary_tree_nodes.c
+++ 13-binary_tree_nodes.c
@@ -9,13 +9,14 @@
 
 size_t binary_tree_nodes(const binary_tree_t *tree)
 {
+	size_t nodes = 0;
+
 	if (tree == NULL)
 		return (0);
 
-	if (tree->left == NULL || tree->right == NULL)
-		return (1 + binary_tree_nodes(tree->left) + binary_tree_nodes
-				(tree->right));
+	nodes += (tree->left || tree->right) ? 1 : 0;
+	nodes += binary_tree_nodes(tree->left);
+	nodes += binary_tree_nodes(tree->right);
 
-	else
-		return (0);
+	return (nodes);
 }
